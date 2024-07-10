ff954c1a022df79e0ea61f164627e1ac15392f01
Sinan <muhammedsinan7749@gmail.com>
2024-07-10 16:34:15 +0530

    working

--- server/routers/routes.js
+++ server/routers/routes.js
@@ -12,17 +12,17 @@ router.put('/teams/:id', updateTeam);       // PUT /api/teams/:id
 router.delete('/teams/:id', deleteTeam);    // DELETE /api/teams/:id
 
 // Program routes
-router.get('/programs', getPrograms);                   // GET /api/programs
-router.post('/programs', addProgram);                   // POST /api/programs
-router.put('/programs/set-winner', setWinners)          // PUT /api/programs
-router.put('/programs/:id', updateProgram);             // PUT /api/programs/:id
-router.delete('/programs/:id', deleteProgram);          // DELETE /api/programs/:id
+router.get('/programs', getPrograms);             // GET /api/programs
+router.post('/programs', addProgram);             // POST /api/programs
+router.put('/programs/set-winner', setWinners)    // PUT /api/programs
+router.put('/programs/:id', updateProgram);       // PUT /api/programs/:id
+router.delete('/programs/:id', deleteProgram);    // DELETE /api/programs/:id
 
 // Participant routes
-router.get('/participants', getParticipants);                                       // GET /api/participants
-router.post('/participants', addParticipant);                                       // POST /api/participants
-router.put('/participants/add-program/:id', addProgramToParticipant);              // POST /api/participants
-router.put('/participants/:id', updateParticipant);                                 // PUT /api/participants/:id
-router.delete('/participants/:id', deleteParticipant);                              // DELETE /api/participants/:id
+router.get('/participants', getParticipants);                             // GET /api/participants
+router.post('/participants', addParticipant);                             // POST /api/participants
+router.put('/participants/add-program/:id', addProgramToParticipant);     // POST /api/participants
+router.put('/participants/:id', updateParticipant);                       // PUT /api/participants/:id
+router.delete('/participants/:id', deleteParticipant);                    // DELETE /api/participants/:id
 
 export default router;
