2e74c950feed28dc13dce7ccc0ed39717a848f9e
Sinan <muhammedsinan7749@gmail.com>
2024-07-21 00:09:30 +0530

    deploying on render

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: "http://localhost:4000",
+        target: "https://localhost:4000",
         changeOrigin: true
       }
     }
--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 import express, { urlencoded, json } from 'express'
 import cookieParser from 'cookie-parser';
+import path from 'path'
 import { connectDB } from './config/mongo.js';
 import { router } from './routers/routes.js';
 import { errorHandler } from './middleware/errorHandler.js'
@@ -15,6 +16,14 @@ app.use(urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.use('/api', router);
+
+if (process.env.NODE_ENV==='production') {
+    const __dirname = path.resolve();
+    app.use(express.static(path.join(__dirname,'client/dist')))
+    app.get('*',(req,res)=>res.sendFile(path.resolve(__dirname,'client','dist','index.html')));
+}else{
+    app.get('/',(req,res)=>res.send("server is ready"))
+}
 
 app.use(errorHandler);
 
