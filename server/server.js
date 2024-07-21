fdfaa6b3a747f011711f8a546e089d51802dcd6e
Sinan <muhammedsinan7749@gmail.com>
2024-07-21 06:11:33 +0530

    cors added in server

--- server/server.js
+++ server/server.js
@@ -1,9 +1,10 @@
-import express, { urlencoded, json } from 'express'
+import express, { urlencoded, json } from 'express';
 import cookieParser from 'cookie-parser';
-import path from 'path'
+import path from 'path';
+import cors from 'cors';
 import { connectDB } from './config/mongo.js';
 import { router } from './routers/routes.js';
-import { errorHandler } from './middleware/errorHandler.js'
+import { errorHandler } from './middleware/errorHandler.js';
 import { configDotenv } from 'dotenv';
 
 configDotenv();
@@ -11,23 +12,27 @@ connectDB();
 
 const app = express();
 
+app.use(cors());
+
 app.use(json());
 app.use(urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.use('/api', router);
 
-if (process.env.NODE_ENV==='production') {
+if (process.env.NODE_ENV === 'production') {
     const __dirname = path.resolve();
-    app.use(express.static(path.join(__dirname,'client/dist')))
-    app.get('*',(req,res)=>res.sendFile(path.resolve(__dirname,'client','dist','index.html')));
-}else{
-    app.get('/',(req,res)=>res.send("server is ready"))
+    app.use(express.static(path.join(__dirname, 'client/dist')));
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html')));
+} else {
+    app.get('/', (req, res) => res.send("Server is ready"));
 }
 
 app.use(errorHandler);
 
-const port = process.env.PORT
+const port = process.env.PORT || 2000;
 app.listen(port, () => {
-    console.log(`listening on port 2000 http://localhost:${port}/`);
+    console.log(`Server is running on port ${port} http://localhost:${port}/`);
 });
+
+export default app;
