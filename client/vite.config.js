9bf6eefa740908efbea4cd094167e3074feda5af
Sinan <muhammedsinan7749@gmail.com>
2024-07-21 05:48:10 +0530

    deploying client  on render

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: "https://localhost:4000",
+        target: "https://sahityotsav-manager.onrender.com",
         changeOrigin: true
       }
     }
