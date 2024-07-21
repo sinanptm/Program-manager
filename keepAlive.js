139680c210d65bda9037b35936c057c4aa4abaa4
Sinan <muhammedsinan7749@gmail.com>
2024-07-21 18:04:21 +0530

    Added keep-alive script and updated package.json

--- keepAlive.js
+++ keepAlive.js
@@ -2,7 +2,7 @@ import fetch from 'node-fetch';
 import cron from 'node-cron';
 
 const keepAlive = () => {
-  fetch('https://your-server-url.com')
+  fetch('https://sahityotsav-manager.onrender.com')
     .then(res => console.log(`Keep-alive ping: ${res.status}`))
     .catch(err => console.error(`Error pinging server: ${err.message}`));
 };
